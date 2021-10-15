6729acbc0e11f0d15ff3f0169f1942ba28207a39
Muhaimen Ahmed <muhaimen.ahmed@gmail.com>
2021-10-15 11:35:08 -0400

    updated tag-routes.js, fixed syntax errors in the api folder

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   try {
     const categoriesData = await Category.destroy({
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productsData = await Product.findByPk(req.params.id, {
@@ -113,8 +113,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productsData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productsData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
